31aa947464b34fb5066a359439dae3f211b4a85f
Maycon Queiroz <fmaycon@gmail.com>
2023-03-18 14:30:46 -0300

    feat(webworker): create worker to process list card

--- pages/titles/src/factories/cardsFactory.js
+++ pages/titles/src/factories/cardsFactory.js
@@ -1,14 +1,22 @@
-import CardsController from "./../controllers/cardsController.js"
-import CardsView from "./../views/cardsView.js"
-import CardsService from "./../services/cardsService.js"
-const [rootPath] = window.location.href.split('/pages/')
+import CardsController from "./../controllers/cardsController.js";
+import CardsView from "./../views/cardsView.js";
+import CardsService from "./../services/cardsService.js";
+const [rootPath] = window.location.href.split("/pages/");
+
+const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, {
+  type: "module"
+});
+
 const factory = {
   async initialize() {
     return CardsController.initialize({
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
-    })
+      service: new CardsService({
+        cardListWorker,
+        dbUrl: `${rootPath}/assets/database.json`
+      })
+    });
   }
-}
+};
 
-export default factory
+export default factory;
